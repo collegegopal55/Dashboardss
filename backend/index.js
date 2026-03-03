71bec2994b4e0fe67362cc5c2491ee5a55293611
collegegopal55 <collegegopal55@gmail.com>
2026-03-03 17:09:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -232,7 +232,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [ 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5000','https://dashboardss-e7ez.onrender.com' ],
+  origin: [ 'https://dashboardss-e7ez.onrender.com' ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
