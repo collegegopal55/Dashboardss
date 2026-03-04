c0ccd61efb96b1938e5a37edda89c49d0b7f0c67
collegegopal55 <collegegopal55@gmail.com>
2026-03-04 08:18:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -557,8 +557,8 @@ app.get("/api/health", (req, res) => {
 const path = require("path");
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist","index.html"));
 });
 
 // Server Start
